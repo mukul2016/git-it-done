bde8b802085df66786e2c8fb82de82c9df4bf1b0
Mukul Sharma <sh.mukul@gmail.com>
2020-06-23 16:33:11 -0700

    Add an Extra Message

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector('#issues-container')
+var limitWarningEl = document.querySelector('#limit-warning')
 
 var getRepoIssues = function (repo) {
   var apiUrl = 'https://api.github.com/repos/' + repo + '/issues?direction=asc'
@@ -7,6 +8,10 @@ var getRepoIssues = function (repo) {
     if (response.ok) {
       response.json().then(function (data) {
         displayIssues(data)
+        // check if api has paginated issues
+        if (response.headers.get('Link')) {
+          console.log('repo has more than 30 issues')
+        }
       })
     } else {
       alert('There was a problem with your request!')
@@ -17,10 +22,7 @@ var getRepoIssues = function (repo) {
 }
 
 var displayIssues = function (issues) {
-  if (issues.length === 0) {
-    issueContainerEl.textContent = 'This repo has no open issues!'
-    return
-  }
+  displayWarning(repo)
 
   for (var i = 0; i < issues.length; i++) {
     // create a link element to take users to the issue on github
@@ -49,6 +51,18 @@ var displayIssues = function (issues) {
     issueEl.appendChild(typeEl)
     issueContainerEl.appendChild(issueEl)
   }
+}
+
+var displayWarning = function (repo) {
+  // add text to warning container
+  limitWarningEl.textContent = 'To see more than 30 issues, visit '
+  var linkEl = document.createElement('a')
+  linkEl.textContent = 'See More Issues on GitHub.com'
+  linkEl.setAttribute('href', 'https://github.com/' + repo + '/issues')
+  linkEl.setAttribute('target', '_blank')
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl)
 }
 
 getRepoIssues('facebook/react')
